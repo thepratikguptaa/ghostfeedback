cf68cf04cbf0d97632e7addb328939b8e6a25595
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-20 01:07:50 +0530

    style: update Navbar component layout and styling for improved consistency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,17 @@ const Navbar = () => {
     
     return (
     <nav className='px-6 py-4 shadow-sm bg-white sticky top-0 z-50'>
-        <div className='max-w-7xl mx-auto flex flex-col items-center justify-between md:flex-row gap-6'>
-            <a className='text-2xl font-semibold text-gray-900 hover:text-gray-700 transition-colors' href="#">Ghost Feedback</a>
+        <div className='max-w-container mx-auto flex flex-col items-center justify-between md:flex-row gap-6'>
+            <a className='text-2xl font-semibold text-gray-900 hover:text-gray-700 transition-colors font-[Poppins]' href="#">Ghost Feedback</a>
             {
                 session ? (
                     <>
-                    <span className='hidden md:block text-gray-600 font-medium flex-grow text-center'>Welcome, {user?.username || user?.email}</span>
-                    <Button className='w-full md:w-auto bg-gray-900 hover:bg-gray-700 transition-colors' onClick={() => signOut()}>Logout</Button>
+                    <span className='hidden md:block text-gray-600 font-medium flex-grow text-center mr-40'>Welcome, {user?.username || user?.email}</span>
+                    <Button className='w-full md:w-auto bg-gray-900 hover:bg-gray-700 transition-colors cursor-pointer' onClick={() => signOut()}>Logout</Button>
                     </>
                 ) : (
                     <Link className='cursor-pointer' href="/sign-in">
-                        <Button className='w-full md:w-auto bg-gray-900 hover:bg-gray-700 transition-colors'>Login</Button>
+                        <Button className='w-full md:w-auto bg-gray-900 hover:bg-gray-700 transition-colors cursor-pointer'>Login</Button>
                     </Link>
                 )
             }
