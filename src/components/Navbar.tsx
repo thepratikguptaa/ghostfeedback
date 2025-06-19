14e11b0eccb05e606986deae3967f2d46f795038
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-19 20:28:27 +0530

    style: enhance Navbar component with improved styling and layout adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,18 +11,18 @@ const Navbar = () => {
     const user: User = session?.user as User
     
     return (
-    <nav className='p-4 md:p-6 shadow-md'>
-        <div className='container mx-auto flex flex-col items-center justify-between md:flex-row'>
-            <a className='text-2xl font-bold mb-2 mb:mb-0' href="#">Ghost Feedback</a>
+    <nav className='p-4 md:p-6 shadow-md bg-white'>
+        <div className='container mx-auto flex flex-col items-center justify-between md:flex-row gap-4'>
+            <a className='text-2xl font-bold mb-2 md:mb-0 text-gray-800 hover:text-gray-600' href="#">Ghost Feedback</a>
             {
                 session ? (
                     <>
-                    <span className='mr-4 hidden md:block justify-center items-center'>Welcome, {user?.username || user?.email}</span>
-                    <Button className='w-full md:w-auto' onClick={() => signOut()}>Logout</Button>
+                    <span className='mr-4 hidden md:block justify-center items-center text-gray-700'>Welcome, {user?.username || user?.email}</span>
+                    <Button className='w-full md:w-auto cursor-pointer' onClick={() => signOut()}>Logout</Button>
                     </>
                 ) : (
-                    <Link href="/sign-in">
-                        <Button className='w-full md:w-auto'>Login</Button>
+                    <Link className='cursor-pointer' href="/sign-in">
+                        <Button className='w-full md:w-auto cursor-pointer'>Login</Button>
                     </Link>
                 )
             }
