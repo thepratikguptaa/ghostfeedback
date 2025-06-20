46bf82e690c9d75806e657cd4383521d82ec23f1
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-20 15:33:33 +0530

    updated the loader

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -92,7 +92,11 @@ const dashboard = () => {
 
   const username = session?.user?.username
   if (!username) {
-    return <div>User information not available</div>
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="animate-spin h-16 w-16" />
+      </div>
+    )
   }
   const baseUrl = `${window.location.protocol}//${window.location.host}`
   const profileUrl = `${baseUrl}/u/${username}`
